1fa9b1c9d1688cee608fb51b409293a55aea280f
Fábio Oliveira <fabio20rj@gmail.com>
2020-05-29 10:27:11 -0300

    Modified the model core

--- core/models.py
+++ core/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Log(models.Model):
     log = models.CharField(max_length=200)
+    level = models.CharField(max_length=20)
+    event = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
