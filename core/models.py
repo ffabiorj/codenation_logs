3ca9d76b2343bdf729c7c9a365b3b731b7aeaee9
Fábio Oliveira <fabio20rj@gmail.com>
2020-06-13 10:14:44 -0300

    Added new atribute in the model.

--- core/models.py
+++ core/models.py
@@ -5,7 +5,11 @@ class Log(models.Model):
     log = models.CharField(max_length=200, blank=False, null=False)
     level = models.CharField(max_length=20, blank=False, null=False)
     event = models.IntegerField(blank=False, null=False)
+    archived = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def archived_true(self):
+        self.archived = True
 
     class Meta:
         ordering = ["-created_at"]
