5d696f3b54bbd4d982c16102cb688b880e3339df
Fábio Oliveira <fabio20rj@gmail.com>
2020-06-17 09:54:05 -0300

    fixed test_model

--- core/tests/test_model.py
+++ core/tests/test_model.py
@@ -6,12 +6,14 @@ import pytest
 def test_set_archived_for_true():
     log = Log.objects.create(log="Teste", level="warning", event=100)
     log.archived_true()
-    assert log.archived == True
+    assert log.archived is True
+
 
 @pytest.mark.django_db
 def test_str_of_log():
     log = Log.objects.create(log="Teste", level="warning", event=100)
     assert log.__str__() == "Teste"
+
 
 @pytest.mark.django_db
 def test_len_of_log_equal_one():
