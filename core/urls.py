4e9d308e16e74ef94f85ce5df85604f8c0966cbe
Fábio Oliveira <fabio20rj@gmail.com>
2020-06-24 14:54:33 -0300

    feat: added search log in api.

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from core import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path("logs/", views.LogList.as_view(), name="logs"),
-    path("logs/<int:pk>/", views.LogDetail.as_view(), name="log"),
+    path("log/", views.LogList.as_view(), name="log"),
+    path("log/<int:pk>/", views.LogDetail.as_view(), name="log"),
+    path("logs/", views.LogSearch.as_view(), name="search_logs"),
     path("token/", jwt_views.TokenObtainPairView.as_view(), name="token"),
     path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,11 +3,24 @@ from core.models import Log
 from core.serializers import LogSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, filters
 from rest_framework.permissions import IsAuthenticated
 
 
-class LogList(APIView):
+class LogSearch(generics.ListAPIView):
+    """
+    Search for logs.
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    search_fields = ["log"]
+    filter_backends = (filters.SearchFilter,)
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
+
+
+class LogList(generics.ListCreateAPIView):
 
     """
     List all logs, or create a new log.
@@ -15,17 +28,8 @@ class LogList(APIView):
 
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        logs = Log.objects.all()
-        serializer = LogSerializer(logs, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = LogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
 
 
 class LogDetail(APIView):
